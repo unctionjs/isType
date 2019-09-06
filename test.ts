7148be3057ae928f910e5f43be9f188bfd85cdf9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:47 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,42 +1,42 @@
 /* eslint-disable no-magic-numbers, no-undefined */
 import isType from "./index";
 
-test(() => {
+test("works", () => {
   expect(isType("null")(null)).toBeTruthy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("undefined")(undefined)).toBeTruthy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("String")(null)).toBeFalsy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("String")(undefined)).toBeFalsy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("String")("b")).toBeTruthy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("String")("")).toBeTruthy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("Object")({})).toBeTruthy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("Array")([])).toBeTruthy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("Number")(1)).toBeTruthy();
 });
 
-test(() => {
+test("works", () => {
   expect(isType("Buffer")(new Buffer("x"))).toBeTruthy();
 });
